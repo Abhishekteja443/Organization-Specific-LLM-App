7140c42091f08516e9fd1fe891c43a0ddfec6eff
Abhishekteja443 <abhishekteja45@gmail.com>
2025-03-09 18:29:31 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Oragnization Specific LLM App",
+    author="Abhishek Teja",
+    author_email="g.abhishekteja@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
